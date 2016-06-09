e741a3fbee4ed6f0ac879c0741bd73d3dfa49d95
Troy Denkinger <tdenkinger@gmail.com>
2016-06-09 08:11:14 -0400

    Day 1, Puzzle 1: Determine the final floor arrived at

--- elixir/advent/lib/advent_day1.ex
+++ elixir/advent/lib/advent_day1.ex
@@ -1,2 +1,17 @@
 defmodule AdventDay1 do
+  def final_floor(starting_position, move_list) do
+    move_list
+    |> String.split("", trim: :true)
+    |> track_floors(starting_position)
+  end
+
+  defp track_floors([], position), do: position
+
+  defp track_floors([h | t], position) when h == "(" do
+    track_floors(t, position + 1)
+  end
+
+  defp track_floors([h | t], position) when h == ")" do
+    track_floors(t, position - 1)
+  end
 end
