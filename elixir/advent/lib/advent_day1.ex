8aab96f688aec69555cec101674ad47996c6f62d
Troy Denkinger <tdenkinger@gmail.com>
2016-06-12 10:10:46 -0400

    Day 1, Part 2: determine when the moves first go below zero.

--- elixir/advent/lib/advent_day1.ex
+++ elixir/advent/lib/advent_day1.ex
@@ -5,6 +5,24 @@ defmodule AdventDay1 do
     |> track_floors(starting_position)
   end
 
+  def enter_basement(move_list) do
+    move_list
+    |> String.split("", trim: :true)
+    |> find_basement_move(0, 0)
+  end
+
+  defp find_basement_move(_, position, floor) when floor < 0, do: position
+
+  defp find_basement_move([h | t], position, floor) when h == "(" do
+     find_basement_move(t, position + 1, floor + 1)
+  end
+
+  defp find_basement_move([h | t], position, floor) when h == ")" do
+     find_basement_move(t, position + 1, floor - 1)
+  end
+
+  defp find_basement_move([], _, _), do: {:error, :never_entered_basement}
+
   defp track_floors([], position), do: position
 
   defp track_floors([h | t], position) when h == "(" do
