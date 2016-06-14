20e873662915a27786b51266b77b8e7d369022fe
Troy Denkinger <tdenkinger@gmail.com>
2016-06-14 08:37:39 -0400

    WIP refactoring

--- elixir/advent/lib/advent_day2.ex
+++ elixir/advent/lib/advent_day2.ex
@@ -1,24 +1,47 @@
 defmodule AdventDay2 do
   def get_paper_required(dimensions) do
     dimensions
-    |> String.split("\n", trim: :true)
-    |> Enum.map(fn(x) -> parse_dimensions(x) end)
-    |> Enum.reduce(0, fn(dims, acc) -> calculate_areas(dims, acc) end)
+    |> extract_package_dimensions
+    |> Enum.reduce(0, fn(dims, acc) -> calculate_package_areas(dims, acc) end)
   end
 
-  def parse_dimensions(dimensions) do
-    String.split(dimensions, "x")
+  def get_ribbon_required(dimensions) do
+    dimensions
+    |> extract_package_dimensions
+    |> Enum.reduce(0, fn(dims, acc) -> calculate_ribbon_length(dims, acc) end)
+  end
+
+  defp extract_package_dimensions(package_list) do
+    package_list
+    |> String.split("\n", trim: :true)
+    |> Enum.map(fn(x) -> parse_dimensions(x) end)
+  end
+
+  defp parse_dimensions(dimensions) do
+    dimensions
+    |> String.split("x")
     |> Enum.map(fn(x) -> String.to_integer(x) end)
     |> Enum.sort
   end
 
-  def calculate_areas(dimensions, total_area) do
-    total_area + calculate_raw_area(dimensions) + calculate_extra(dimensions)
+  defp calculate_ribbon_length(dimensions, total_length) do
+    total_length +
+    calculate_raw_length(dimensions) +
+    calculate_extra_length(dimensions)
   end
 
-  def calculate_raw_area([l, w, h]) do
-    2 * (( l * w) + (w * h) + (h * l))
+
+  defp calculate_package_areas(dimensions, total_area) do
+    total_area +
+    calculate_raw_area(dimensions) +
+    calculate_extra_area(dimensions)
   end
 
-  def calculate_extra([a, b, _]), do: a * b
+  defp calculate_raw_length([l, w, _]), do: (2 * l) + (2 * w)
+
+  defp calculate_extra_length([l, w, h]), do: l * w * h
+
+  defp calculate_raw_area([l, w, h]), do: 2 * (( l * w) + (w * h) + (h * l))
+
+  defp calculate_extra_area([a, b, _]), do: a * b
 end
--- elixir/advent/test/advent_day2_test.exs
+++ elixir/advent/test/advent_day2_test.exs
@@ -17,5 +17,22 @@ defmodule AdventDay2Test do
     paper_total = AdventDay2.get_paper_required(data)
     assert paper_total == 1586300
   end
+
+  test "figures the amount of ribbon for one package" do
+    ribbon_total = AdventDay2.get_ribbon_required("2x4x3")
+    assert ribbon_total == 34
+  end
+
+  test "figures the amount of ribbon for more than one package" do
+    ribbon_total = AdventDay2.get_ribbon_required("2x4x3\n3x2x4")
+    assert ribbon_total == 68
+  end
+
+  test "figures the amount of ribbon for all packages" do
+    {:ok, data} = File.read("../../input/day2_input")
+    paper_total = AdventDay2.get_ribbon_required(data)
+    assert paper_total == 3737498
+  end
+
 end
 
