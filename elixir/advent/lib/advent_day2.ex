830041623a19e6c5b4a60dece1f01325b6c62052
Troy Denkinger <tdenkinger@gmail.com>
2016-06-14 08:15:05 -0400

    Day 2, Exercise 1 finished.

--- elixir/advent/lib/advent_day2.ex
+++ elixir/advent/lib/advent_day2.ex
@@ -9,6 +9,7 @@ defmodule AdventDay2 do
   def parse_dimensions(dimensions) do
     String.split(dimensions, "x")
     |> Enum.map(fn(x) -> String.to_integer(x) end)
+    |> Enum.sort
   end
 
   def calculate_areas(dimensions, total_area) do
@@ -16,7 +17,7 @@ defmodule AdventDay2 do
   end
 
   def calculate_raw_area([l, w, h]) do
-    (2 * l * w) + (2 * w * h) + (2 * h * l)
+    2 * (( l * w) + (w * h) + (h * l))
   end
 
   def calculate_extra([a, b, _]), do: a * b
--- elixir/advent/test/advent_day2_test.exs
+++ elixir/advent/test/advent_day2_test.exs
@@ -3,19 +3,19 @@ defmodule AdventDay2Test do
   doctest AdventDay2
 
   test "figures the amount of paper for one package" do
-    paper_total = AdventDay2.get_paper_required("2x3x4")
+    paper_total = AdventDay2.get_paper_required("4x3x2")
     assert paper_total == 58
   end
 
   test "figures the amount of paper for more than one package" do
-    paper_total = AdventDay2.get_paper_required("2x3x4\n2x3x4")
+    paper_total = AdventDay2.get_paper_required("2x3x4\n2x4x3")
     assert paper_total == 116
   end
 
   test "figures the amount of paper for all packages" do
     {:ok, data} = File.read("../../input/day2_input")
     paper_total = AdventDay2.get_paper_required(data)
-    assert paper_total == 1
+    assert paper_total == 1586300
   end
 end
 
