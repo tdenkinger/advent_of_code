401f51c987f6cb3857ebb1b4808792ddf19eb6ed
Troy Denkinger <tdenkinger@gmail.com>
2016-06-19 09:59:35 -0400

    Day 4, part 2 completed with a little refctoring.

--- elixir/advent/lib/advent_day4.ex
+++ elixir/advent/lib/advent_day4.ex
@@ -1,18 +1,18 @@
 defmodule AdventDay4 do
-  def run(secret) do
-    find_coin(false, secret, 0)
+  def run(secret, size) do
+    find_coin(false, secret, size, 0)
   end
 
-  def find_coin(true, _, suffix), do: suffix
+  def find_coin(true, _, _, suffix), do: suffix
 
-  def find_coin(false, secret, suffix) do
+  def find_coin(false, secret, size, suffix) do
     suffix = suffix + 1
     create_hash(secret, suffix)
-    |> test_hash_for_coin
-    |> find_coin(secret, suffix)
+    |> test_hash_for_coin(size)
+    |> find_coin(secret, size, suffix)
   end
 
-  def test_hash_for_coin(hash), do: Regex.match?(~r/^0{5}/, hash)
+  def test_hash_for_coin(hash, size), do: Regex.match?(~r/^0{#{size}}/, hash)
 
   def create_hash(secret, suffix) do
     :crypto.hash(:md5, "#{secret}#{suffix}")
--- elixir/advent/test/advent_day4_test.exs
+++ elixir/advent/test/advent_day4_test.exs
@@ -2,8 +2,13 @@ defmodule AdventDay4Test do
   use ExUnit.Case
   doctest AdventDay4
 
-  test "finds the first adventcoin" do
-    coin = AdventDay4.run('ckczppom')
-    assert coin == 609043
+  test "finds the first five-zero adventcoin" do
+    coin = AdventDay4.run('ckczppom', 5)
+    assert coin == 117946
+  end
+
+  test "finds the first six-zero adventcoin" do
+    coin = AdventDay4.run('ckczppom', 6)
+    assert coin == 3938038
   end
 end
