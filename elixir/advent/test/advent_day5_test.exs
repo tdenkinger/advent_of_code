0904e3044bb08310ffebda98e2485826ebda6cf2
Troy Denkinger <tdenkinger@gmail.com>
2016-06-20 10:33:51 -0400

    Add tests that exercise the various conditions through the public function.

--- elixir/advent/test/advent_day5_test.exs
+++ elixir/advent/test/advent_day5_test.exs
@@ -2,6 +2,26 @@ defmodule AdventDay5Test do
   use ExUnit.Case
   doctest AdventDay5
 
+  test "find nice names in a one item list" do
+    count = AdventDay5.count_nice_names("aaceijo")
+    assert count == 1
+  end
+
+  test "does not find nice names when there aren't enough vowels" do
+    count = AdventDay5.count_nice_names("tyyyya")
+    assert count == 0
+  end
+
+  test "does not find nice names when containing a naughty pair" do
+    count = AdventDay5.count_nice_names("abeeci")
+    assert count == 0
+  end
+
+  test "does not find nice names when no repating letters" do
+    count = AdventDay5.count_nice_names("aybeci")
+    assert count == 0
+  end
+
   test "find nice names on a long list" do
     {:ok, data} = File.read("../../input/day5_input")
     count = AdventDay5.count_nice_names(data)
