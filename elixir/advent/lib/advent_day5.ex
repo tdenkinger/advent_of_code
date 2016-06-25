e8d56019a3d1f40d2f2cdfc57d4d2e65fad0d26d
Troy Denkinger <tdenkinger@gmail.com>
2016-06-25 10:08:37 -0400

    Correctly finds non-repeating pairs of the _same_ letters (aa, bb, xx), but that's not the requirement. It was supposed to find any pair repeated (qj & qj)

--- elixir/advent/lib/advent_day5.ex
+++ elixir/advent/lib/advent_day5.ex
@@ -1,5 +1,35 @@
-defmodule AdventDay5.Improved do
+defmodule AdventDay5 do
+  def split_name(name) do
+    name |> String.split("", trim: true)
+  end
+end
 
+defmodule AdventDay5.Improved do
+  def test_pairs(name_list), do: test_pairs(name_list, 0)
+
+  def test_pairs([], acc), do: acc
+
+  def test_pairs([h | t], acc) do
+    cond do
+      next_pair_matches?(h, t) -> test_pairs(remove_matched_char(t, h), acc + 1)
+      true -> test_pairs(t, acc)
+    end
+  end
+
+  defp next_pair_matches?(letter, letters) do
+    letter == Enum.at(letters, 0)
+  end
+
+  defp remove_matched_char(letters, match) do
+    List.delete(letters, match)
+  end
+
+  def repeating_pairs(name) do
+    name
+    |> AdventDay5.split_name
+    |> test_pairs
+    |> IO.inspect
+  end
 end
 
 defmodule AdventDay5.Original do
@@ -21,18 +51,14 @@ defmodule AdventDay5.Original do
   end
 
   defp has_enough_vowels?(name) do
-    split_name(name)
+    AdventDay5.split_name(name)
     |> Enum.filter(fn(letter) -> String.contains?("aeiou", letter) end)
     |> Enum.count >= 3
   end
 
-  defp split_name(name) do
-    name |> String.split("", trim: :true)
-  end
-
   defp has_repeated_letters?(name) do
-    total_count   = split_name(name) |> Enum.count
-    deduped_count = split_name(name) |> Enum.dedup |> Enum.count
+    total_count   = AdventDay5.split_name(name) |> Enum.count
+    deduped_count = AdventDay5.split_name(name) |> Enum.dedup |> Enum.count
 
     deduped_count < total_count
   end
--- elixir/advent/test/advent_day5_test.exs
+++ elixir/advent/test/advent_day5_test.exs
@@ -1,42 +1,66 @@
 defmodule AdventDay5Test do
+  use ExUnit.Case, async: true
+  doctest AdventDay5
 
-  defmodule ImprovedVersion do
-    use ExUnit.Case
-    doctest AdventDay5
+  ExUnit.configure exclude: :pending, trace: true
 
+  describe "New and improved version" do
     alias AdventDay5.Improved
 
-    test "true" do
-      # assert true
+    test "has more than one repeating pair" do
+      count = Improved.repeating_pairs("qjhvhtzxzqq")
+      assert count == 2
+    end
+
+    test "has a single repeated pair of letters" do
+      count = Improved.test_pairs(["x","x"])
+      assert count == 1
+    end
+
+    test "has a single un-overlapping repeated pair of letters" do
+      count = Improved.test_pairs(["x","x","x"])
+      assert count == 1
+    end
+
+    test "has multiple repeated pairs of letters" do
+      count = Improved.test_pairs(["x","x","y","y"])
+      assert count == 2
+    end
+
+    test "has separate, multiple repeated pairs of letters" do
+      count = Improved.test_pairs(["x","x","a","y","y"])
+      assert count == 2
     end
   end
 
-  defmodule OriginalVersion do
-    use ExUnit.Case
-    doctest AdventDay5
-
+  describe "Original version" do
     alias AdventDay5.Original
 
+    @tag :pending
     test "find nice names in a one item list" do
       count = Original.count_nice_names("aaceijo")
       assert count == 1
     end
 
+    @tag :pending
     test "does not find nice names when there aren't enough vowels" do
       count = Original.count_nice_names("tyyyya")
       assert count == 0
     end
 
+    @tag :pending
     test "does not find nice names when containing a naughty pair" do
       count = Original.count_nice_names("abeeci")
       assert count == 0
     end
 
+    @tag :pending
     test "does not find nice names when no repating letters" do
       count = Original.count_nice_names("aybeci")
       assert count == 0
     end
 
+    @tag :pending
     test "find nice names on a long list" do
       {:ok, data} = File.read("../../input/day5_input")
       count = Original.count_nice_names(data)
